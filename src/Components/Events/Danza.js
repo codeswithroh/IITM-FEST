cccc567ff9614a4e4b820110e5460e9c5c0d2a25
Ravi Chaudhary <malhotraravi842@gmail.com>
2021-07-21 12:46:05 +0530

    updated

--- src/Components/Events/Danza.js
+++ src/Components/Events/Danza.js
@@ -1,7 +1,6 @@
 import EventPage from "../EventPage";
 function Danza() {
   return (
-<<<<<<< HEAD
     <EventPage title="Danza" image='danza.png' date='24 July 2021' link='https://forms.gle/qSpZCyCcnNvs4xe46'>
     <li>The time limit for each entry will be 1.5-3 minutes.</li><br />
     <li>The theme should be strictly Eastern or Western depending on the category that the participant is choosing.</li><br />
@@ -10,53 +9,6 @@ function Danza() {
     <li>Winner will be selected on the basis of People's choice.</li><br />
     
   </EventPage>
-||||||| 1ddb556
-    <div>
-      <div className='homepage-navigation'>
-        <Link to='/'>
-          <div className='homepage_logo'></div>
-        </Link>
-      </div>
-      <EventPage title="Danza" image='danza.png'>
-        
-    </EventPage>
-    <div class='registration-buttons'>
-        <div class='register'>
-          <a href='https://forms.gle/qSpZCyCcnNvs4xe46'>
-            <span id='registration'>Register Now</span>
-            <div class='liquid'></div>
-          </a>
-        </div>
-      </div>
-    </div>
-=======
-    <div>
-      <div className='homepage-navigation'>
-        <Link to='/'>
-          <div className='homepage_logo'></div>
-        </Link>
-      </div>
-      <EventPage title='Danza' image='danza.png'></EventPage>
-      <div className='button-container'>
-        <div className='registration-buttons'>
-          <div className='register'>
-            <a href='https://forms.gle/9TsmAG2Rp5dbBroZA'>
-              <span id='registration'>Register Now</span>
-              <div className='liquid other-liquid'></div>
-            </a>
-          </div>
-        </div>
-        <div className='registration-buttons'>
-          <div className='register'>
-            <a href='https://forms.gle/9TsmAG2Rp5dbBroZA'>
-              <span id='registration'>More Info</span>
-              <div className='liquid'></div>
-            </a>
-          </div>
-        </div>
-      </div>
-    </div>
->>>>>>> codeswithroh-master
   );
 }
 
--- src/Components/Events/WarOfWords.js
+++ src/Components/Events/WarOfWords.js
@@ -1,57 +1,9 @@
 import EventPage from "../EventPage";
 function WarOfWords() {
   return (
-<<<<<<< HEAD
     <EventPage title="War Of Words" image='debate.png' date='24 July' link='https://forms.gle/fbGKNMySUyQ1xRSL8'>
         
     </EventPage>
-||||||| 1ddb556
-    <div>
-      <div className='homepage-navigation'>
-        <Link to='/'>
-          <div className='homepage_logo'></div>
-        </Link>
-      </div>
-      <EventPage title="War Of Words" image='debate.png'>
-        
-    </EventPage>
-    <div class='registration-buttons'>
-        <div class='register'>
-          <a href='https://forms.gle/fbGKNMySUyQ1xRSL8'>
-            <span id='registration'>Register Now</span>
-            <div class='liquid'></div>
-          </a>
-        </div>
-      </div>
-    </div>
-=======
-    <div>
-      <div className='homepage-navigation'>
-        <Link to='/'>
-          <div className='homepage_logo'></div>
-        </Link>
-      </div>
-      <EventPage title='War Of Words' image='debate.png'></EventPage>
-      <div className='button-container'>
-        <div className='registration-buttons'>
-          <div className='register'>
-            <a href='https://forms.gle/9TsmAG2Rp5dbBroZA'>
-              <span id='registration'>Register Now</span>
-              <div className='liquid other-liquid'></div>
-            </a>
-          </div>
-        </div>
-        <div className='registration-buttons'>
-          <div className='register'>
-            <a href='https://forms.gle/9TsmAG2Rp5dbBroZA'>
-              <span id='registration'>More Info</span>
-              <div className='liquid'></div>
-            </a>
-          </div>
-        </div>
-      </div>
-    </div>
->>>>>>> codeswithroh-master
   );
 }
 
